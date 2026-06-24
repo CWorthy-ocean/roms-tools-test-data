fe7fd4dfa132462628167e4dcb544eed910a2a01
Kristen Thyng <kthyng@gmail.com>
2026-06-24 12:49:39 -0700

    Change BGC dataset path and update drop list
    Updated the input BGC dataset file path and modified the drop list to exclude 'temp_WOA' and 'salt_WOA'.

--- processing_scripts/coarsen_unified_bgc.py
+++ processing_scripts/coarsen_unified_bgc.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # This script is used to coarsen the current UNIFIED BGC dataset to make a new coarse dataset used for testing.
 
-path_2_bgc = './BGCdataset.nc'
+path_2_bgc = './BGCdataset_v2_filled.nc'  # just tracking which file we are at for the test file
 path_2_coarsened_bgc = './coarsened_UNIFIED_bgc_dataset.nc'
 
 # Load in both datasets
@@ -28,7 +28,7 @@ bgc_interp=bgc_interp.isel(dep=slice(0,2))
 list_interp = list(bgc_interp.data_vars)
 list_coarse = list(bgc_coarse.data_vars)
 
-drop_list = list(set(list_interp)-set(list_coarse))
+drop_list = list(set(list_interp)-set(list_coarse) - set(['temp_WOA','salt_WOA']))
 bgc_interp = bgc_interp.drop_vars(drop_list)
 
 # Save to netCDF to overwrite the old one 
